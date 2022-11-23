be6e74e6a95e744496a9d5dfc87a4e8a29b19f2c
cmayo76 <47158530+cmayo76@users.noreply.github.com>
2022-11-23 01:02:34 -0600

    11232022 102am

--- HTML Files for COP2000/HTML files/html10/tutorial/lht_calendar.js
+++ HTML Files for COP2000/HTML files/html10/tutorial/lht_calendar.js
@@ -40,6 +40,7 @@ document.getElementById("calendar").innerHTML = createCalendar(thisDay);
 function createCalendar(calDate) {
     var calendarHTML = "<table id='calendar_table'>";
     calendarHTML += calCaption(calDate);
+    calendarHTML += calWeekdayRow();
     calendarHTML += "</table>";
     return calendarHTML;
 }
@@ -58,4 +59,53 @@ function calCaption(calDate) {
 
     // Write the caption
     return "<caption>" + monthName[thisMonth] + " " + thisYear + "</caption>";
+}
+
+
+/* Function to write a table row of weekday abbreviations */
+function calWeekdayRow () {
+    //Array of weekday abbreviations
+    var dayName = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
+    var rowHTML = "<tr>";
+
+    // Loop through the dayName array
+    for (var i = 0; i < dayName.length; i ++) {
+        rowHTML += "<th class='calendar_weekdays'>" + dayName[i]  + "</th>"
+    }
+    rowHTML += "</tr>";
+    return rowHTML;
+}
+
+/* Function to calculate the number of days in the month */
+function daysInMonth(calDate) {
+    //Array of days in each month
+    var dayCount = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+    // Extract the four digit year and month value
+    var thisYear = calDate.getFullYear();
+    var thisMonth = calDate.getMonth();
+
+    // Revise the days in February for leap year
+    if (thisYear % 4 === 0) {
+        if ((thisYear % 100 != 0) || (thisYear % 400 === 0)) {
+        dayCount[1] = 29 }
+    }
+
+    // Return the number of days for the current month
+    return dayCount[thisMonth];
+}
+
+/* Function to write table rows for each day of the month */
+function calDays(calDate) {
+    // Determine the starting day of the month
+    var day = new Date(calDate.getFullYear(), calDate.getMonth(), 1);
+    var weekDay = day.getDay();
+
+    // Write blank cells preceding the starting day
+    var htmlCode = "<tr>";
+    for (var i = 0; i < weekDay; i++) {
+        htmlCode += "<td></td>";
+    }
+
+    // Write cells for each day of the month
 }
