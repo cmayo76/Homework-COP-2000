9cf559b546e351c5a322ca4be7a5f7d5cb175b20
cmayo76 <47158530+cmayo76@users.noreply.github.com>
2022-11-28 21:41:44 -0600

    Java Course

---
+++ Java/Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/1. Getting Started/Challenge 1/YourInitials.java
@@ -0,0 +1,13 @@
+public class YourInitials {
+    public static void main(String[] args) {
+         // Instructions for this workbook are on Learn the Part (See the Udemy Video: Your Initials to access the link).
+        System.out.println(" CCC    M   M");
+        System.out.println("C   C   MM MM");
+        System.out.println("C       M M M");
+        System.out.println("C       M   M");
+        System.out.println("C       M   M");
+        System.out.println("C   C   M   M");
+        System.out.println(" CCC    M   M");
+
+        }
+}
